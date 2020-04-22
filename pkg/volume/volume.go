c3721725adb77bb094ebfcea9ad1cd9bb5a7c520
Alberto Negron <albertonegron@gmail.com>
2020-04-22 18:34:07 +0100

    add better comments to volume pkg

--- pkg/volume/volume.go
+++ pkg/volume/volume.go
@@ -4,14 +4,17 @@ import (
 	"math"
 )
 
+//Shape works a placeholder for various geometrical shapes
 type Shape interface {
 	getVolume() float64
 }
 
+//Volume will calculate the volume of a 3-d object.
 func Volume(s Shape) float64 {
 	return s.getVolume()
 }
 
+//Cube contains the params required to calculate the volume of a cube
 type Cube struct {
 	Side float64
 }
@@ -20,7 +23,7 @@ func (c Cube) getVolume() float64 {
 	return math.Pow(c.Side, 3)
 }
 
-//RectPrism is a Rectangular prism
+//RectanglePrism contains the params required to calculate the volume of a rectangle prism
 type RectanglePrism struct {
 	Length float64
 	Width  float64
@@ -31,7 +34,7 @@ func (r RectanglePrism) getVolume() float64 {
 	return r.Length * r.Width * r.Height
 }
 
-//TrianPrism is a Triangular prism
+//TrianglePrism contains the params required to calculate the volume of a triangle prism
 type TrianglePrism struct {
 	Base   float64
 	Height float64
@@ -42,7 +45,7 @@ func (t TrianglePrism) getVolume() float64 {
 	return t.Base * t.Length * t.Height / 2
 }
 
-//Cylinder
+//Cylinder contains the params required to calculate the volume of a cylinder
 type Cylinder struct {
 	Radius float64
 	Height float64
@@ -52,6 +55,7 @@ func (c Cylinder) getVolume() float64 {
 	return math.Pi * math.Pow(c.Radius, 2) * c.Height
 }
 
+//Pyramid contains the params required to calculate the volume of a pyramid
 type Pyramid struct {
 	Length float64
 	Width  float64
@@ -62,7 +66,7 @@ func (p Pyramid) getVolume() float64 {
 	return p.Length * p.Width * p.Height / 3
 }
 
-//Cone
+//Cone contains the params required to calculate the volume of a cone
 type Cone struct {
 	Radius float64
 	Height float64
@@ -72,7 +76,7 @@ func (c Cone) getVolume() float64 {
 	return math.Pi * math.Pow(c.Radius, 2) * c.Height / 3
 }
 
-//Sphere
+//Sphere contains the params required to calculate the volume of a sphere
 type Sphere struct {
 	Radius float64
 }
